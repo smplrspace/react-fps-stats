1fd420e11dbea12dc61e185d21a76aa5d0a6b98b
Leon Fedotov <fedotov.leon@gmail.com>
2022-03-04 19:23:14 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,17 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
@@ -42,8 +48,8 @@ function FPSStats(_ref) {
       bottom = _ref$bottom === void 0 ? 'auto' : _ref$bottom,
       _ref$left = _ref.left,
       left = _ref$left === void 0 ? 0 : _ref$left,
-      _ref$graphHeight = _ref.graphHeight,
-      graphHeight = _ref$graphHeight === void 0 ? 29 : _ref$graphHeight,
+      _ref$barHeight = _ref.barHeight,
+      barHeight = _ref$barHeight === void 0 ? 29 : _ref$barHeight,
       _ref$graphWidth = _ref.graphWidth,
       graphWidth = _ref$graphWidth === void 0 ? 70 : _ref$graphWidth;
 
@@ -91,7 +97,7 @@ function FPSStats(_ref) {
   var fps = state.fps,
       max = state.max,
       len = state.len;
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react["default"].createElement("div", {
     style: {
       zIndex: 999999,
       position: 'fixed',
@@ -111,24 +117,24 @@ function FPSStats(_ref) {
       bottom: bottom,
       left: left
     }
-  }, /*#__PURE__*/React.createElement("span", null, fps[len - 1], " FPS"), /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react["default"].createElement("span", null, fps[len - 1], " FPS"), /*#__PURE__*/_react["default"].createElement("div", {
     style: {
       position: 'absolute',
       left: 3,
       right: 3,
       bottom: 3,
-      height: graphHeight,
+      height: barHeight,
       background: '#282844',
       boxSizing: 'border-box'
     }
   }, fps.map(function (frame, i) {
-    return /*#__PURE__*/React.createElement("div", {
+    return /*#__PURE__*/_react["default"].createElement("div", {
       key: "fps-".concat(i),
       style: {
         position: 'absolute',
         bottom: 0,
         right: "".concat(len - 1 - i, "px"),
-        height: "".concat(graphHeight * frame / max, "px"),
+        height: "".concat(barHeight * frame / max, "px"),
         width: 1,
         background: '#00ffff',
         boxSizing: 'border-box'
